8d90b32a34310d13d87510f10d80d7e95e128f8e
sergeytura <sergei.pashkov@list.ru>
2023-02-08 17:34:26 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from './components/App';
+import NewTaskForm from "./components/NewTaskForm";
+import TaskList from "./components/TaskList";
+import TasksFilter from "./components/TasksFilter";
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const App = () => {
+    return (
+        <section className="todoapp">
+                <NewTaskForm />
+            <section className="main">
+                <TaskList />
+                <TasksFilter />
+            </section>
+        </section>
+    );
+}
+
 root.render(<App />)
